1d683c18c321b20822206aaaa3ea7774d1bc2421
Dev Singh <sigmaplus128@gmail.com>
2026-04-14 15:53:12 +0530

    Delete frontend directory

--- frontend/src/components/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-const AuthLayout = ({ children }) => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="w-full max-w-md">{children}</div>
-    </div>
-  );
-};
-
-export default AuthLayout;
